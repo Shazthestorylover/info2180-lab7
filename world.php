c5b195ad80cd72c9bf5a6c7db6c7ebaafd4133a8
Ubuntu <ubuntu@ip-172-31-27-153.ec2.internal>
2019-11-21 00:58:13 +0000

    first commit

--- world.php
+++ world.php
@@ -13,6 +13,32 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 //var_dump($results);
 
 ?>
+
+<table>
+    <!-- <caption>List of Students</caption> -->
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Continent</th>
+            <th>Independence</th>
+            <th>Head of State</th>
+        </tr>
+    </thead>
+    
+    <tbody>
+        <?php foreach ($results as $row): ?>
+        <tr>
+            <td><?= $row['name']; ?></td>
+            <td><?= $row['continent']; ?></td>
+            <td><?= $row['independence_year']; ?></td>
+            <td><?= $row['head_of_state']; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+
+
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
